7b96b91e6e137df8259c7a13298bf350239328ee
Shray Sharma <shray.sharma@mail.utoronto.ca>
2017-03-23 16:07:13 -0400

    Init map moved to external js file, generates markers based on list of buildings.

--- assets/js/allBuildings.js
+++ assets/js/allBuildings.js
@@ -1,54 +1,99 @@
-// jQuery Document
-var map, marker;
+/* 
+ * Most of this code was adapted from:
+ * https://wrightshq.com/playground/placing-multiple-markers-on-a-google-map-using-api-3/
+ *
+ * Building list for testing purposes:
+ *      var markers = [
+            ['Bahen Center', 43.659643, -79.397668],
+            ['Galbraith Building', 43.660131, -79.395993],
+            ['McLennan Physical Laboratories', 43.660879, -79.398444]
+        ];
+ */
 
 $(document).ready(function() {
+    console.log('Document ready.');
+
+    // global list of building markers
+    var markers = [
+        ['Bahen Center', 43.659643, -79.397668],
+        ['Galbraith Building', 43.660131, -79.395993],
+        ['McLennan Physical Laboratories', 43.660879, -79.398444]
+    ];
+
+    // generate building list cards and marker list
+    getBuildings();
+
+    function initMap() {
+        var map;
+        var bounds = new google.maps.LatLngBounds();
+        var mapOptions = {
+            mapTypeId: 'roadmap'
+        };
+
+        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        map.setTilt(45);
+
+        // place each marker on the map
+        var marker, i;
+        for (i = 0; i < markers.length; i++) {
+            var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+            bounds.extend(position);
+            marker = new google.maps.Marker({
+                position: position,
+                map: map,
+                title: markers[i][0]
+            });
+
+            // Automatically center the map fitting all markers on the screen
+            map.fitBounds(bounds);
+        }
+
+        // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+        var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+            this.setZoom(16);
+            google.maps.event.removeListener(boundsListener);
+        });
+    }
+
+    /* Ajax call to get buildings */
+    function getBuildings() {
+        $.ajax({
+            url: '/api/v1/building/get_all_buildings',
+            method: "GET",
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: function(data) {
+                var buildings = data;
+                if (!buildings.length) {
+                    console.log('No buildings found.');
+                    return;
+                }
+
+                var txt = "";
+                var name, address;
+                for (var i = 0; i < buildings.length; i++) {
+                	// store name, slice address to keep only number and street name
+                    name = buildings[i].name;
+                    address = buildings[i].address.slice(0, buildings[i].address.indexOf(','));
+
+                    // create list-group-item with building info
+                    txt += '<a class="list-group-item" href="' + name + '-info.html">' 
+                    	+ '<div class="row"><div class="col-sm-9">' 
+                    	+ '<h4 class="card-heading list-group-item-heading">' + name + '</h4>' 
+                    	+ '<p class="list-group-item-text">' + address + '</p></div>'
+                    	+ '<div class="card-icon col-sm-3">' 
+                    	+ '<i class="fa fa-angle-right fa-3x pull-right" aria-hidden="true"></i></div></div></a>'
+                    
+                    // create marker for this building and push to list
+                    markers.push([buildings[i].name, buildings[i].lat, buildings[i].lon]);
+                }
+
+                // append building list cards to list-group
+                $("#list-group").append(txt).removeClass("hidden");
+            }
+        });
+    }
+
+    // create map with building markers
     initMap();
 });
-
-function initMap() {
-    var uluru = { lat: 43.660411, lng: -79.396959 };
-    map = new google.maps.Map(document.getElementById('map'),, {
-        zoom: 4,
-        center: uluru
-    });
-    marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-    });
-}
-
-<a class="list-group-item" href="#">
-        <div class="row">
-          <div class="col-sm-9">
-            <h4 class="card-heading list-group-item-heading">Bahen Center</h4>
-            <p class="list-group-item-text">17 Free Rooms</p>
-          </div>
-          <div class="card-icon col-sm-3">
-            <i class="fa fa-angle-right fa-3x pull-right" aria-hidden="true"></i>
-          </div>
-        </div>
-      </a>
-
-/* Ajax call to get buildings */
-function getbuildings() {
-    $.ajax({
-        url: '/api/v1/building/get_all_buildings',
-        method: "GET",
-        dataType: "json",
-        contentType: "application/json; charset=utf-8",
-        success: function(data) {
-        	var buildings = data;
-        	if (!buildings.length) {
-        		console.log('No buildings found.');
-        	}
-
-        	var txt = "";
-        	for (var i = 0; i < buildings.length; i++) {
-        		txt += '<a class="list-group-item" href="' + buildings[i].name + '-info.html">'
-        			+ '<div class="row"><div class="col-sm-9">'
-        			+ '<h4 class="card-heading list-group-item-heading">' + buildings[i].name + '</h4>'
-        			+ '<p class="list-group-item-text">' + buildings[i].address +
-        	}
-        }
-    });
-}
