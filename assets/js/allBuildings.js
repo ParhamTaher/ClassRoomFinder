2349340fcac9bf956dc4aec9b49c924bbe957d86
Shray Sharma <shray.sharma96@gmail.com>
2017-03-23 02:38:36 -0400

    Random syntax changes.

--- assets/js/allBuildings.js
+++ assets/js/allBuildings.js
@@ -1,26 +1,30 @@
 // jQuery Document
+var map, marker;
+
 $(document).ready(function() {
-
-    function initMap() {
-        var uluru = { lat: 43.660411, lng: -79.396959 };
-        var map = new google.maps.Map($('#map'), {
-            zoom: 4,
-            center: uluru
-        });
-        var marker = new google.maps.Marker({
-            position: uluru,
-            map: map
-        });
-    }
-
-    /* Ajax call to get buildings */
-    function getbuildings() {
-        $.ajax({
-            url: '/api/v1/building/get_all_buildings',
-            method: "GET",
-            dataType: "json",
-            contentType: "application/json; charset=utf-8",
-            success: function(data) {}
-        });
-    }
+    initMap();
 });
+
+function initMap() {
+    var uluru = { lat: 43.660411, lng: -79.396959 };
+    map = new google.maps.Map(document.getElementById('map'),, {
+        zoom: 4,
+        center: uluru
+    });
+    marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
+
+/* Ajax call to get buildings */
+function getbuildings() {
+    $.ajax({
+        url: '/api/v1/building/get_all_buildings',
+        method: "GET",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function(data) {
+        }
+    });
+}
