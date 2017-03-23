0e7a70d4fcf863008317b16fea0cb040feeaaecb
Shray Sharma <shray.sharma96@gmail.com>
2017-03-23 01:28:19 -0400

    Created building cards and map but not dynamically.

--- assets/js/allBuildings.js
+++ assets/js/allBuildings.js
@@ -1,4 +1,26 @@
 // jQuery Document
 $(document).ready(function() {
 
+    function initMap() {
+        var uluru = { lat: 43.660411, lng: -79.396959 };
+        var map = new google.maps.Map($('#map'), {
+            zoom: 4,
+            center: uluru
+        });
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+        });
+    }
+
+    /* Ajax call to get buildings */
+    function getbuildings() {
+        $.ajax({
+            url: '/api/v1/building/get_all_buildings',
+            method: "GET",
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: function(data) {}
+        });
+    }
 });
