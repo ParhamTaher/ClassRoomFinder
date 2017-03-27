2a23bec45558bfac98182c14592751425d1af8d3
Parham Taher <parham.taher@gmail.com>
2017-03-27 19:25:48 -0400

    cleaning up loadrooms js file

--- assets/js/loadRooms.js
+++ assets/js/loadRooms.js
@@ -1,75 +1,21 @@
-var buildingName = "";
 var roomNumber = "";
 
-function loadRooms() {
-
-}
-
-function loadLabs() {
-
-}
-
-function getBuilingNameFromBtnClicked() {
-  return
-}
-
-
 $(document).ready(function() {
-  /*
-  $('.list-group-item').click(function() {
-      buildingName = $(this).attr("name");
-      //alert(buildingName);
+    $('.roomBtn').click(function() {
 
-  });
-
-  $('.rooms-sub-nav').click(function() {
-      //alert("Room!");
-      $('#building_rooms').removeClass("hidden");
-      $('#map-canvas').addClass("hidden");
-
-      $(".buildingName").html(buildingName + " -> Rooms");
-  });
-
-  $('.labs-sub-nav').click(function() {
-      //alert("Lab!");
-      $('#building_rooms').removeClass("hidden");
-      $('#map-canvas').addClass("hidden");
-
-      $(".buildingName").html(buildingName + " -> Labs");
-  });
-
-  $('.lecture-sub-nav').click(function() {
-      //alert("Lecture Hall!");
-      $('#building_rooms').removeClass("hidden");
-      $('#map-canvas').addClass("hidden");
-
-      $(".buildingName").html(buildingName + " -> Lecture Halls");
-  });
-  */
-
-  $('.roomBtn').click(function() {
-
-      roomNumber = $(this).html()
-      //alert(roomNumber);
-      $('#room_info').removeClass("hidden");
-      $('#building_rooms').addClass("hidden");
-
-      //alert(buildingName);
-
+        roomNumber = $(this).html()
+        //alert(roomNumber);
+        $('#room_info').removeClass("hidden");
+        $('#building_rooms').addClass("hidden");
   });
 
   $('#commentBtn').click(function() {
       $('#comments_section').removeClass("hidden");
       $('#room_info').addClass("hidden");
-
-      //alert(buildingName);
-
   });
 
 
   $('#submitComment').click(function() {
-    //alert($('#commentText').val());
     $('#comments').append('<div class="comment">' + $('#commentText').val() + '</div>')
-
   });
 });
